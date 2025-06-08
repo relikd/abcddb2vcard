8bdaba0322ac02b60f3bf080a730ed9b97c153cf
relikd <info@relikd.de>
2025-06-09 00:33:47 +0200

    fix: py min version 3.6 due to f-string usage

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     },
     long_description_content_type="text/markdown",
     long_description=longdesc,
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     keywords=[
         'abcddb',
         'abcd',
@@ -41,7 +41,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
